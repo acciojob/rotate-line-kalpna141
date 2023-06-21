a77c484382776918886cec781fdcdff583f258e7
Kalpna <132220523+kalpna141@users.noreply.github.com>
2023-06-21 18:09:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-function rotate(){
+const line=document.getElementById('line');
+let rotation=0;
+setInterval(()=>{
+	rotation+=5;
+	line.style.transform=`translate(-50%, -50%) rotate(${rotation}deg)`;
+},50);
 
-    let timing = document.getElementById("line");
-    let degree = 0;
 
-    function rotateAtInterval(){
-        degree = degree + 5;
-        timing.style.transform = `rotate(${degree}deg)`;
-        timing.style.transitionTimingFunction = 'linear';
-        timing.style.transitionDuration = '0.4s'; 
+
+// function rotate(){
+
+//     let timing = document.getElementById("line");
+//     let degree = 0;
+
+//     function rotateAtInterval(){
+//         degree = degree + 5;
+//         timing.style.transform = `rotate(${degree}deg)`;
+//         timing.style.transitionTimingFunction = 'linear';
+//         timing.style.transitionDuration = '0.4s'; 
         
-    }
+//     }
 
-    setInterval(rotateAtInterval,100)
+//     setInterval(rotateAtInterval,100)
 
-}
+// }
 
-rotate();
+// rotate();
